f1dd6e8d1e87723e532625595d60b397d23ff442
Danny Damen <danny@bits2byte.com>
2023-07-16 16:35:48 +0200

    Exercises 4 & 5 added

---
+++ Exercise_4/CE4_Make_some_variables.py
@@ -0,0 +1,15 @@
+# Define a variable named city and set it equal to any string
+
+city = "Amsterdam"
+
+# Define a variable named price and set it equal to any float
+
+price = 3.29
+
+# Define a variable named high_score and set it equal to any int
+
+high_score = 9001
+
+# Define a variable named is_having_fun and set it to a Boolean value
+
+is_having_fun = True
---
+++ Exercise_5/CE5_Escape_Sequence_Practice.py
@@ -0,0 +1,14 @@
+ # Set the message variable equal to any string containing a new-line escape sequence
+message = "Here starts \n a new line"
+print(message)
+
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+print(mountains)
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = "And he said \"Nay, we are but men. ROCK!\""
+print(quotation)
